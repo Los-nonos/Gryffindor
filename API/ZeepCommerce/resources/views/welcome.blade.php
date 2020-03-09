81eda10a0a0219f2234b8c3c2bb2116e78ec2069
MartinMontanari <maritnmontanari3@gmail.com>
2020-03-09 10:04:51 -0300

    Add dependencies

--- API/ZeepCommerce/resources/views/welcome.blade.php
+++ API/ZeepCommerce/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ZeepCommerce Dashboard & API
                 </div>
 
                 <div class="links">
