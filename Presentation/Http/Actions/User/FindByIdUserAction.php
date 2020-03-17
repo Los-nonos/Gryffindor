fe4ce868d3dda8be1c624d40632d480340c8153d
cristianvena18 <cristiandamianvena@gmail.com>
2020-03-16 21:54:51 -0300

    intent add routes but not

---
+++ Presentation/Http/Actions/User/FindByIdUserAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Presentation\Http\Actions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FindByIdUserAction extends Controller {
+    public function execute($id) {
+        
+    }
+}
