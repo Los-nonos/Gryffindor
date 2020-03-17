8354af998a32bfaeb97feef6abe93791fd9d2b3c
cristianvena18 <cristiandamianvena@gmail.com>
2020-03-16 23:51:32 -0300

    added store route

---
+++ Presentation/Http/Actions/User/StoreUserAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Presentation\Http\Actions;
+
+use Presentation\Http\Actions\Controller;
+use Illuminate\Http\Request;
+
+class StoreUserAction extends Controller {
+    public function execute() {
+        
+    }
+}
