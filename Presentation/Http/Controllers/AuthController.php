2f9b813f440eb06ad68b66c6a2fd9ad4a5adcf94
cristianvena18 <cristiandamianvena@gmail.com>
2020-03-26 21:18:04 -0300

    fix error email to change by username

--- Presentation/Http/Controllers/AuthController.php
+++ Presentation/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->only('username', 'password');
 
         if ($token = $this->guard()->attempt($credentials)) {
             return $this->respondWithToken($token);
