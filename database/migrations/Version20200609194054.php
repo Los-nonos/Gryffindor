75ae80e653fe0a70e41e69e8d7b1a524282d8883
cristianvena18 <cristiandamianvena@gmail.com>
2020-06-09 20:04:57 -0300

    fix migrations

--- database/migrations/Version20200609194054.php
+++ database/migrations/Version20200609194054.php
@@ -15,10 +15,10 @@ class Version20200609194054 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255), description VARCHAR(255), price FLOAT, taxes FLOAT, category_id INTEGER, stock_id INTEGER, characteristic_id INTEGER,  PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), filter_id INTEGER, products_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE filters (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), options_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), filter_id INTEGER, product_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE filters (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), category_id INTEGER, options_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE characteristics (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), property VARCHAR(255), product_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE filter_options (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE filter_options (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255), filter_id INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE orders (id INT AUTO_INCREMENT NOT NULL, products_id INTEGER, customer_id INTEGER, seller_id INTEGER, amount FLOAT, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE stocks (id INT AUTO_INCREMENT NOT NULL, product_id INTEGER, quantity INTEGER, remanentQuantity INTEGER, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
@@ -26,7 +26,7 @@ class Version20200609194054 extends AbstractMigration
         $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_AA5A118EA76ED313 FOREIGN KEY (stock_id) REFERENCES stocks (id)');
         $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_AA5A118EA76ED314 FOREIGN KEY (category_id) REFERENCES categories (id)');
         $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_AA5A118EA76ED315 FOREIGN KEY (filter_id) REFERENCES filters (id)');
-        $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_AA5A118EA76ED316 FOREIGN KEY (products_id) REFERENCES products (id)');
+        $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_AA5A118EA76ED316 FOREIGN KEY (product_id) REFERENCES products (id)');
         $this->addSql('ALTER TABLE filters ADD CONSTRAINT FK_AA5A118EA76ED317 FOREIGN KEY (options_id) REFERENCES filter_options (id)');
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_AA5A118EA76ED318 FOREIGN KEY (products_id) REFERENCES products (id)');
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_AA5A118EA76ED319 FOREIGN KEY (customer_id) REFERENCES users (id)');
