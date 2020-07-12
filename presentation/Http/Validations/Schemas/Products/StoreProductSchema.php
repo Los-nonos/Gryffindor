1db21820678b959cb385b79b6a7138d6c23bd940
Martin <martinmontanari3@gmail.com>
2020-07-11 21:35:32 -0300

    Fix StoreProductSchema validation

--- presentation/Http/Validations/Schemas/Products/StoreProductSchema.php
+++ presentation/Http/Validations/Schemas/Products/StoreProductSchema.php
@@ -12,7 +12,7 @@ class StoreProductSchema
     public function getRules(): array
     {
         return [
-            'name' => 'bail|required|regex:/^[\pL\s]+$/u|min:10|max:256',
+            'name' => 'bail|required|min:10|max:256',
             'description' => 'bail|required|min:10|max:2700',
             'price' => 'bail|required|numeric|regex:/^\d*(\.\d{2})?$/|min:0.01',
             'categories' => 'bail|required|array',
@@ -33,11 +33,9 @@ class StoreProductSchema
 
         return [
             'name.required' => 'Debe ingresar el nombre del producto',
-            'name.regex' => 'El nombre del producto solo puede contener números y letras',
             'name.min' => 'El nombre del producto debe tener 10 carácteres como mínimo',
             'name.max' => 'El nombre del producto debe tener 256 carácterés como máximo',
             'description.required'  => 'Debe ingresar la descripción del producto',
-            'description.regex'  => 'Revise el formato de la descripcion del producto',
             'description.min' => 'La descripción del producto debe tener 10 carácteres como mínimo',
             'description.max'  => 'La descripción del producto debe tener 2700 carácterés como máximo',
             'price.required' => 'Debe ingresar el precio del producto',
@@ -52,7 +50,6 @@ class StoreProductSchema
             'iva.numeric' => 'El IVA ingresado debe ser un número',
             'iva.min' => 'El IVA mínimo permitido es 0%',
             'brand.required' => 'Debe ingresar una marca para el producto',
-            'brand.regex' => 'La marca ingresada para el producto contener solo números y letras',
             'brand.min' => 'La marca ingresada debe tener al menos 1 caracter',
             'characteristics.required' => 'Debe ingresar las características del producto',
             'order.alpha_num' => 'La orden ingresada solo debe contener números y letras',
