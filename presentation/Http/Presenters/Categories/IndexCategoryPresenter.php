c175591c60e3211bf5b44b1a5fde5209694b1263
cristianvena18 <cristiandamianvena@gmail.com>
2020-07-17 14:36:14 -0300

    deleted logger

--- presentation/Http/Presenters/Categories/IndexCategoryPresenter.php
+++ presentation/Http/Presenters/Categories/IndexCategoryPresenter.php
@@ -29,7 +29,6 @@ class IndexCategoryPresenter
                 'filters' => $this->clearFilters($category->getFilters()),
             ]);
         }
-        logger($cleanData);
         return $cleanData;
     }
 
