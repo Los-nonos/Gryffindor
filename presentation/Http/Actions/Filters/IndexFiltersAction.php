6d8d2b560b198b4b2d5e0919e2920d40f9c66e3c
cristianvena18 <cristiandamianvena@gmail.com>
2020-07-20 15:04:18 -0300

    added search providers in index filters

--- presentation/Http/Actions/Filters/IndexFiltersAction.php
+++ presentation/Http/Actions/Filters/IndexFiltersAction.php
@@ -6,12 +6,14 @@ namespace Presentation\Http\Actions\Filters;
 
 use Application\Queries\Query\Brands\IndexBrandsQuery;
 use Application\Queries\Query\Categories\IndexCategoryQuery;
+use Application\Queries\Query\Providers\IndexProvidersQuery;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Infrastructure\QueryBus\QueryBusInterface;
 use Presentation\Http\Enums\HttpCodes;
 use Presentation\Http\Presenters\Brands\IndexBrandsPresenter;
 use Presentation\Http\Presenters\Categories\IndexCategoryPresenter;
+use Presentation\Http\Presenters\Providers\IndexProvidersPresenter;
 
 class IndexFiltersAction
 {
@@ -21,15 +23,19 @@ class IndexFiltersAction
 
     private IndexBrandsPresenter $brandsPresenter;
 
+    private IndexProvidersPresenter $providersPresenter;
+
     public function __construct(
         QueryBusInterface $queryBus,
         IndexCategoryPresenter $categoryPresenter,
-        IndexBrandsPresenter $brandsPresenter
+        IndexBrandsPresenter $brandsPresenter,
+        IndexProvidersPresenter $providersPresenter
     )
     {
         $this->queryBus = $queryBus;
         $this->categoryPresenter = $categoryPresenter;
         $this->brandsPresenter = $brandsPresenter;
+        $this->providersPresenter = $providersPresenter;
     }
 
     public function __invoke(Request $request)
@@ -46,11 +52,17 @@ class IndexFiltersAction
 
         $brands = $this->brandsPresenter->fromResult($brandsResult)->getData();
 
+        $providersQuery = new IndexProvidersQuery(1, 100);
+
+        $providersResult = $this->queryBus->handle($providersQuery);
+
+        $providers = $this->providersPresenter->fromResult($providersResult)->getData();
+
         return new JsonResponse([
             'data' => [
                 'categories' => $categories,
                 'brands' => $brands,
-                'providers' => [['id' => 1, 'name' => 'El kiosquito de la esquina']]
+                'providers' => $providers
             ]],
             HttpCodes::OK
         );
